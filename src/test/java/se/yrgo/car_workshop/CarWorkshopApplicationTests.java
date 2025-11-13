3540c61f2462cc1e1a6ca5973d0941f4e4c3d068
André Ljungsvik <140394392+andreljungsvik@users.noreply.github.com>
2025-11-13 14:06:06 +0100

    first commit

---
+++ src/test/java/se/yrgo/car_workshop/CarWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.yrgo.car_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
